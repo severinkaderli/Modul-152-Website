23aaf804c9e89f3a7d741487e859c39562003d60
Severin Kaderli <severin.kaderli@gmail.com>
2017-06-07 20:34:32 +0200

    Random fixes
    Signed-off-by: Severin Kaderli <severin.kaderli@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,36 @@
 $(window).on("load", function() {
 	// Add exif data about camera settings to each image
 	$("img").each(function() {
-		var el = $(this).get(0);
-		EXIF.getData(el, function() {
+		var image = $(this).get(0);
+
+		$(this).parent().append(`
+			<h5>Bildinformation</h5>
+			<table class="card-text">
+				<tr>
+					<th>Blende:</th>
+					<td class="aperture"></td>
+				</tr>
+				<tr>
+					<th>Belichtung:</th>
+					<td class="exposure"></td>
+				</tr>
+				<tr>
+					<th>ISO:</th>
+					<td class="iso"></td>
+				</tr>
+			</table>
+		`);
+		
+		EXIF.getData(image, function() {
 			var iso = EXIF.getTag(this, "ISOSpeedRatings");
 			var FNumber = EXIF.getTag(this, "FNumber");
 			var exposure = EXIF.getTag(this, "ExposureTime");
 			$(this).parent().find(".iso").html(iso);
+
+			if(iso) {
+
+			}
+
 			if(exposure) {
 				$(this).parent().find(".exposure").html(`${exposure.numerator}/${exposure.denominator}`);
 			}
